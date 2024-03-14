bb54a87df0cb3b49711aa8e6012f7eed80787c3f
Aleksandr Fomichev <aleksandrfomicev260477@gmail.com>
2024-03-14 21:55:41 +0300

    Добавил несколько программ

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,4 +4,209 @@
 // string c = "Hello!";
 
 //"Вывод на экран (в консоль)"
-//Console.WriteLine("Hello,World!");
+// //Console.WriteLine("Hello,World!");
+// using System;
+
+// public class Answer {
+    
+//   static int FindMax(int a, int b, int c)
+//     {
+//       // Введите свое решение ниже:
+//     int max = a;
+//       if (max<b)
+//       {
+//         max = b;
+//       }
+//        if (max<c)
+//        {
+//          max = c;
+//        }
+//       return max;
+
+//     }
+
+//   // Не удаляйте и не меняйте метод Main! 
+//   static public void Main(string[] args) {
+//         int a, b, c;
+
+//         if (args.Length >= 3) {
+//            a = int.Parse(args[0]);
+//            b = int.Parse(args[1]);
+//            c = int.Parse(args[2]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//            a = 5;
+//            b = 6;
+//            c = 7;
+//         }
+
+//         // Не удаляйте строки ниже
+//         int result = FindMax(a, b, c);
+//         System.Console.WriteLine($"{result}");
+//     }
+// }
+
+// using System;
+
+//         public class Answer {
+//  static void CompareNumbers (int firstNumber, int secondNumber);
+//      }
+//         // Введите свое решение ниже
+        
+//                 if (firstNamber , secondNamber)
+//                 {
+//            Console.WriteLine("первое число больше");
+//                         }
+//                  if (firstNamber < secondNamber)
+//                    {
+//                     Console.WriteLine("второе число больше");
+//                         }
+// else
+// {
+//  Console.WriteLine("числа равны");
+// }
+
+//     }
+
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int firstNumber, secondNumber;
+
+//         if (args.Length >= 2) {
+//             firstNumber = int.Parse(args[0]);
+//             secondNumber = int.Parse(args[1]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             firstNumber = 5;
+//             secondNumber = 6;
+//         }
+
+//         // Не удаляйте строки ниже
+//         CompareNumbers(firstNumber, secondNumber);
+//     }
+// }
+
+// using System;
+
+// class Answer {
+//     static void CheckIfEven(int number)
+//     {
+//       // Введите свое решение ниже
+//       int i=2;
+//       while(i <= number)
+//       {
+//         Console.Write(i + " ");
+//         i = i + 2;
+//       }
+
+
+//     }
+  
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int number;
+
+//         if (args.Length >= 1) {
+//             number = int.Parse(args[0]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 6;
+//         }
+
+//         // Не удаляйте строки ниже
+//         CheckIfEven(number);
+//     }
+// }
+
+
+// using System;
+
+// public class Answer {
+//     static void PrintEvenNumbers(int number)
+//     {
+//       // Введите свое решение ниже
+//         int i = 2;
+//         while (i <= number)
+//         {
+//           if ( i % 2 == 0 )
+//          {
+//           Console.Write(i);
+//           Console.Write(' ');
+//           i = i + 2;
+//          }
+//         }
+//     }
+
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int number;
+
+//         if (args.Length >= 1) {
+//             number = int.Parse(args[0]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 8;
+//         }
+
+//         // Не удаляйте строки ниже
+//         PrintEvenNumbers(number);
+//     }
+// }
+
+
+using System;
+
+class Answer {
+    static void CheckIfEven(int number)
+    {
+      // Введите свое решение ниже
+         int i = 0;
+         
+
+         while(i <= number)
+        {
+        
+         if (i % 2 == 0 )
+         {
+           Console.WriteLine(" четное");
+    
+        //  i = i + 1;
+        //  }
+        // else
+        //    {
+        //     Console.WriteLine(" не четное");
+        //    }  
+        }  
+        
+          
+          
+        
+          
+          
+            
+          
+
+          
+        
+
+    }
+  
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        CheckIfEven(number);
+    }
+}}
