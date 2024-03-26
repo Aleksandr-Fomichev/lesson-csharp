730f689ebf35c312697dc38bde6c6d4e680d4d68
Aleksandr Fomichev <aleksandrfomicev260477@gmail.com>
2024-03-26 20:21:31 +0300

    домашнее задание

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,1 +1,75 @@
-﻿
+﻿// //  Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.Write("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
+    {
+       Console.WriteLine("Да");
+    }
+
+
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+
+Console.Write("Введите координаты точки X и Y через пробел:");string[] coordinates = Console.ReadLine().Split(' ');
+int x = Convert.ToInt32(coordinates[0]);
+int y = Convert.ToInt32(coordinates[1]);
+if (x > 0 && y > 0)
+{
+Console.WriteLine("1");
+}
+else if (x < 0 && y > 0)
+{
+Console.WriteLine("2");
+}
+else if (x < 0 && y < 0)
+{
+Console.WriteLine("3");
+}
+else if (x > 0 && y < 0)
+{
+Console.WriteLine("4");
+}
+else
+{
+Console.WriteLine("Точка находится на оси координат");
+}
+
+
+// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine(maxDigit);
+
+
+
+// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{
+    while (N > 0)
+    {
+        int currentDigit = N % 10;
+        N /= 10;
+        if (N > 0)
+        {
+            Console.Write(currentDigit + ",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
